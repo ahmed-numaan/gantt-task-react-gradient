0de77d2adff48e22501ab7d59b26922824fb9a28
Ahmed Numaan <se.numaan@gmail.com>
2023-08-18 15:18:53 +0500

    Fix.

--- src/components/task-item/project/project.tsx
+++ src/components/task-item/project/project.tsx
@@ -6,27 +6,27 @@ export const Project: React.FC<TaskItemProps> = ({ task, isSelected }) => {
   const barColor = isSelected
     ? task.styles.backgroundSelectedColor
     : task.styles.backgroundColor;
-  const processColor = isSelected
-    ? task.styles.progressSelectedColor
-    : task.styles.progressColor;
+  // const processColor = isSelected
+  //   ? task.styles.progressSelectedColor
+  //   : task.styles.progressColor;
   const projectWith = task.x2 - task.x1;
 
-  const projectLeftTriangle = [
-    task.x1,
-    task.y + task.height / 2 - 1,
-    task.x1,
-    task.y + task.height,
-    task.x1 + 15,
-    task.y + task.height / 2 - 1,
-  ].join(",");
-  const projectRightTriangle = [
-    task.x2,
-    task.y + task.height / 2 - 1,
-    task.x2,
-    task.y + task.height,
-    task.x2 - 15,
-    task.y + task.height / 2 - 1,
-  ].join(",");
+  // const projectLeftTriangle = [
+  //   task.x1,
+  //   task.y + task.height / 2 - 1,
+  //   task.x1,
+  //   task.y + task.height,
+  //   task.x1 + 15,
+  //   task.y + task.height / 2 - 1,
+  // ].join(",");
+  // const projectRightTriangle = [
+  //   task.x2,
+  //   task.y + task.height / 2 - 1,
+  //   task.x2,
+  //   task.y + task.height,
+  //   task.x2 - 15,
+  //   task.y + task.height / 2 - 1,
+  // ].join(",");
 
   return (
     <g tabIndex={0} className={styles.projectWrapper}>
